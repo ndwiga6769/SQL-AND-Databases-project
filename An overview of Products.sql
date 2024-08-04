d7540ecc3069db0f15ead2b50a1962870a16afcd
ndwiga6769 <your.email@example.com>
2024-08-04 18:27:43 +0300

    subqueries

--- An overview of Products.sql
+++ An overview of Products.sql
@@ -24,3 +24,32 @@ WHERE
     p.ProductSubcategoryID IS NOT NULL
 ORDER BY 
     SubCategory;
+
+-- 1.2 In 1.1 query you have a product subcategory 
+-- but see that you could use the category name.
+
+-- Find and add the product category name.
+-- Afterwards order the results by Category name.
+SELECT 
+    p.ProductID,
+    p.Name AS ProductName,
+    p.ProductNumber,
+    p.Size,
+    p.Color,
+    p.ProductSubcategoryID,
+    ps.Name AS Subcategory,
+    pc.Name AS Category,
+FROM 
+    tc-da-1.adwentureworks_db.product p
+JOIN 
+    tc-da-1.adwentureworks_db.productsubcategory ps
+ON 
+    p.ProductSubcategoryID = ps.ProductSubcategoryID
+JOIN 
+    tc-da-1.adwentureworks_db.productcategory pc
+ON 
+    ps.ProductCategoryID = pc.ProductCategoryID
+WHERE 
+    p.ProductSubcategoryID IS NOT NULL
+ORDER BY 
+Category;
