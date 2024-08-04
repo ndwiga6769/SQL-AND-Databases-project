d0049011dcdeb6b7ffa50c1dabe95bf45197a3e5
ndwiga6769 <your.email@example.com>
2024-08-04 18:39:57 +0300

    subqueries

--- Reviewing work orders.sql
+++ Reviewing work orders.sql
@@ -6,3 +6,15 @@
 -- For each location Id from the 'workoderrouting' table
 -- for orders in January 2004.
 
+SELECT																									
+w.LocationID,																									
+COUNT(DISTINCT w.WorkOrderID) AS no_work_orders,																									
+COUNT(DISTINCT w.ProductID) AS no_unique_product,																									
+SUM(w.ActualCost) AS actual_cost,																									
+FROM																									
+tc-da-1.adwentureworks_db.workorderrouting w																									
+WHERE																									
+w.ActualStartDate >= '2004-01-01'																									
+AND w.ActualStartDate <= '2004-01-31'	
+AND w.LocationID >= 50																								
+GROUP BY w.LocationID;																									
